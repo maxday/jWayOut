0832445b9f9cf5816e9bda8394c1f2ab1ebb0a26
Maxime David <contact@maximedavid.fr>
2012-05-21 17:32:28 +0000

    compute coord in wall

--- src/Components/Wall.java
+++ src/Components/Wall.java
@@ -1,5 +1,8 @@
 package Components;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Wall {
 
 	private String beginX;
@@ -7,6 +10,8 @@ public class Wall {
 	private String endY;
 	private String endX;
 	private String position;
+	
+	private Map<Long, Long> listCoord = new HashMap<Long, Long>();
 
 	public Wall(String beginX, String beginY, String endX, String endY, String position) {
 		this.beginX = beginX;
@@ -14,7 +19,18 @@ public class Wall {
 		this.endX = endX;
 		this.endY = endY;
 		this.position = position;
-		
+		computeCoord();
+	}
+
+	private void computeCoord() {
+		if("vertical".equals(position)) {
+			for(Long i= Long.parseLong(endY); i<Long.parseLong(endY); ++i)
+				listCoord.put(Long.parseLong(endX), i);
+		}
+		else {
+			for(Long i= Long.parseLong(endX); i<Long.parseLong(endX); ++i)
+				listCoord.put(i, Long.parseLong(endX));
+		}
 	}
 
 	public String getBeginX() {
@@ -39,7 +55,13 @@ public class Wall {
 	
 	public String toString() {
 		return "[BeginX = " + beginX + "; BeginY = " + beginY + "; EndX = " + endX + "; EndY = " + endY + "; Position = " + position + "]";
-
 	}
+
+	public Map<Long, Long> getListCoord() {
+		return listCoord;
+	}
+	
+	
+	
 
 }
